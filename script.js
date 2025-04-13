94ef387a77fa41243ee8e5641f3bf6a9b5b486a6
MARIA GRACIA DOMINGUEZ <2806mariagracia@gmail.com>
2025-04-13 08:36:34 -0500

    Primer commit: subiendo proyecto2

---
+++ script.js
@@ -0,0 +1,18 @@
+const pagoPorHora = 15;
+    const tabla = document.getElementById("tabla");
+    const totalMes = document.getElementById("totalMes");
+    let total = 0;
+
+    document.getElementById("form").addEventListener("submit", function(e) {
+      e.preventDefault();
+      const fecha = document.getElementById("fecha").value;
+      const horas = parseFloat(document.getElementById("horas").value);
+      const totalDia = horas * pagoPorHora;
+      total += totalDia;
+
+      const row = tabla.insertRow();
+      row.innerHTML = `<td>${fecha}</td><td>${horas}</td><td>S/ ${pagoPorHora}</td><td>S/ ${totalDia}</td>`;
+
+      totalMes.innerText = `Total del mes: S/ ${total}`;
+      document.getElementById("form").reset();
+    });
